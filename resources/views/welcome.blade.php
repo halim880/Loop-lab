9531bd8bbb6b3dde638332671d512e4f33881cee
Abdul Halim <a.halimics@gmail.com>
2020-08-12 16:05:00 +0600

    project complete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,239 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
+<nav class="navbar navbar-dark navbar-expand-md fixed-top">
+    <div class="container">
+        <h3 class="navbar-brand">LoopLab</h3>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarMenu">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a class="nav-link" href="#home-section">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="#explore-main-section">Explore</a></li>
+                <li class="nav-item"><a class="nav-link" href="#create-main-section">Create</a></li>
+                <li class="nav-item"><a class="nav-link" href="#share-main-section">Share</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<!-- HOME SECTION -->
+<section id="home-section" style="background-image: url({{asset('image/home.jpg')}})">
+    <div class="dark-overlay">
+        <div class="home-inner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-md-12">
+                        <h1 class="display-4">Build <strong>social profile</strong> gain revenue and <strong>profits</strong></h1>
+                        <div class="d-flex flex-row">
+                            <div class="p-4 align-self-start mt-2"><i class="fa fa-check"></i></div>
+                            <div class="p-4 align-self-end">
+                                <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, iure necessitatibus expedita adipisci aspernatur numquam impedit veritatis delectus possimus aliquam voluptates. Repellat maiores sapiente accusantium.</p>
+                            </div>
+                        </div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                        <div class="d-flex flex-row">
+                            <div class="p-4 align-self-start mt-2"><i class="fa fa-check"></i></div>
+                            <div class="p-4 align-self-end">
+                                <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, iure necessitatibus expedita adipisci aspernatur numquam impedit veritatis delectus possimus aliquam voluptates. Repellat maiores sapiente accusantium.</p>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-row">
+                            <div class="p-4 align-self-start mt-2"><i class="fa fa-check"></i></div>
+                            <div class="p-4 align-self-end">
+                                <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, iure necessitatibus expedita adipisci aspernatur numquam impedit veritatis delectus possimus aliquam voluptates. Repellat maiores sapiente accusantium.</p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="col-lg-4 col-md-8">
+                        <div class="card bg-primary text-center form-card">
+                            <div class="card-body">
+                                <h3>Sign Up Today</h3>
+                                <p>Please fillout the form to register</p>
+                                <form action="">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control form-control-lg" placeholder="Enter your name">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="email" class="form-control form-control-lg" placeholder="Enter your email">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-lg" placeholder="Enter password">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-lg" placeholder="Confirm password">
+                                    
+                                    </div>
+                                    <button class="btn btn-outline-light btn-block">Sign Up</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+</section>
+
+<!-- EXPLORE SECTION -->
+<div id="explore-head-section">
+    <div class="container text-center py-5">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="display-4">Explore</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, iste vero consectetur maxime ut mollitia, qui blanditiis nemo expedita perferendis tempora hic sint voluptas ab a ex architecto molestias corrupti!</p>
+                <a href="find-more" class="btn btn-outline-primary">Find out more</a>
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- EXPLORE MAIN SECTION -->
+<section id="explore-main-section" class="bg-light py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-12">
+                <img src="{{asset('image/home.jpg')}}" class="img-fluid rounded" alt="">
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <h3>Explore Learn & Connect</h3>
+                <p class="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo impedit, vel omnis accusamus excepturi beatae minima, facilis magnam qui tempore quas architecto. Eum perferendis suscipit commodi voluptas nemo fugit repellendus?</p>
+                <div class="d-flex flex-row">
+                    <div class="p-4 align-self-start mt-2"><i class="fa fa-check"></i></div>
+                    <div class="p-4 align-self-end mt-1"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aspernatur voluptatum quidem veniam temporibus vero nemo quae similique beatae quaerat.</p></div>
+
+                </div>
+                <div class="d-flex flex-row">
+                    <div class="p-4 align-self-start mt-2"><i class="fa fa-check"></i></div>
+                    <div class="p-4 align-self-end mt-1"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aspernatur voluptatum quidem veniam temporibus vero nemo quae similique beatae quaerat.</p></div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- CREAATE SECTION -->
+<div id="create-head-section" class="bg-primary">
+    <div class="container text-center py-5">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="display-4">Create</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, iste vero consectetur maxime ut mollitia, qui blanditiis nemo expedita perferendis tempora hic sint voluptas ab a ex architecto molestias corrupti!</p>
+                <a href="find-more" class="btn btn-outline-light">Find out more</a>
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- EXPLORE MAIN SECTION -->
+<section id="create-main-section" class="bg-light py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-12">
+                <h3>Explore Learn & Connect</h3>
+                <p class="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo impedit, vel omnis accusamus excepturi beatae minima, facilis magnam qui tempore quas architecto. Eum perferendis suscipit commodi voluptas nemo fugit repellendus?</p>
+                <div class="d-flex flex-row">
+                    <div class="p-4 align-self-start mt-2"><i class="fa fa-check"></i></div>
+                    <div class="p-4 align-self-end mt-1"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aspernatur voluptatum quidem veniam temporibus vero nemo quae similique beatae quaerat.</p></div>
+
+                </div>
+                <div class="d-flex flex-row">
+                    <div class="p-4 align-self-start mt-2"><i class="fa fa-check"></i></div>
+                    <div class="p-4 align-self-end mt-1"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aspernatur voluptatum quidem veniam temporibus vero nemo quae similique beatae quaerat.</p></div>
+
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <img src="{{asset('image/home.jpg')}}" class="img-fluid rounded" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+   <!-- SHARE SECTION -->
+   <div id="share-head-section" class="bg-primary">
+    <div class="container text-center py-5">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="display-4">Share</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, iste vero consectetur maxime ut mollitia, qui blanditiis nemo expedita perferendis tempora hic sint voluptas ab a ex architecto molestias corrupti!</p>
+                <a href="find-more" class="btn btn-outline-light">Find out more</a>
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- SHARE MAIN SECTION -->
+<section id="share-main-section" class="bg-light py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-12">
+                <img src="{{asset('image/home.jpg')}}" class="img-fluid rounded" alt="">
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <h3>Share What You Create</h3>
+                <p class="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo impedit, vel omnis accusamus excepturi beatae minima, facilis magnam qui tempore quas architecto. Eum perferendis suscipit commodi voluptas nemo fugit repellendus?</p>
+                <div class="d-flex flex-row">
+                    <div class="p-4 align-self-start mt-2"><i class="fa fa-check"></i></div>
+                    <div class="p-4 align-self-end mt-1"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aspernatur voluptatum quidem veniam temporibus vero nemo quae similique beatae quaerat.</p></div>
+
+                </div>
+                <div class="d-flex flex-row">
+                    <div class="p-4 align-self-start mt-2"><i class="fa fa-check"></i></div>
+                    <div class="p-4 align-self-end mt-1"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aspernatur voluptatum quidem veniam temporibus vero nemo quae similique beatae quaerat.</p></div>
+
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+<section id="footer-section" class="text-center text-light py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>LoopLAB</h2>
+                <p>Copyright &copy; 2020</p>
+                <h6>Designed by: <a href="" class="ml-2 text-warning">Abdul Halim</a> </h6>
+                <button class="btn btn-primary btn-sm"
+                data-toggle="modal" data-target="#contactModal">Contact Us</button>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="modal" id="contactModal">
+    <div class="container ">
+        <div class="modal-dialog">
+            <div class="modal-content bg-primary">
+                <div class="modal-header">
+                    <h3>Contact Us</h3>
+                    <button class="btn btn-warning btn-sm">
+                        <span class="close" data-dismiss="modal">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form class="form" action="https://formspree.io/mwkrzqzw" method="post">
+                        <div class="form-group">
+                            <label for="name">Your name</label>
+                            <input id="name" name="name" type="text" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Email</label>
+                            <input type="email" name="_replyto" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Your name</label>
+                            <textarea name="message" id="" cols="30" rows="10" class="form-control"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-outline-light btn-primary">Send</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>   
+@endsection
